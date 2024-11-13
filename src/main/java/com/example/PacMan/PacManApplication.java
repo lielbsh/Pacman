9a6066cf44491134cbf852fa787952cb8cc3a1bc
lielbsh <liel1939@gmail.com>
2024-11-13 14:22:06 +0200

    first commit

---
+++ src/main/java/com/example/PacMan/PacManApplication.java
@@ -0,0 +1,13 @@
+package com.example.PacMan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacManApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacManApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PacMan/PacManApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PacMan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacManApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
