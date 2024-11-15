39cff186bf719b314b05f7b04ef0f66d156d0827
Assaf Assa <assafas141@gmail.com>
2024-11-15 15:11:01 +0200

    figure new concept

--- src/main/java/com/example/PacMan/Figure.java
+++ src/main/java/com/example/PacMan/Figure.java
@@ -3,22 +3,26 @@ package com.example.PacMan;
 import java.util.Map;
 
 public abstract class Figure {
-    protected int index;
-    protected boolean IsPredetor;
-    protected String PhotoHref;
-    protected  int[] coordinates;
-    protected Map<String, int[]> NextStep = Map.of(
+    Map<String, int[]> NEXT_STEP_OPTIONS = Map.of(
             "UP", new int[] {0,1},
             "DOWN", new int[] {0,-1},
             "LEFT", new int[] {-1,0},
             "RIGHT", new int[] {1,0},
             "STAY", new int[] {0,0}
         );
-    
-    protected void move(int[][] boardArray, String nextStep){
-        boardArray[coordinates[0]][coordinates[1]]=0;
-        boardArray[coordinates[0]+NextStep.get(nextStep)[0]][coordinates[1]+NextStep.get(nextStep)[0]]=3;
+
+    protected int index;
+    protected boolean IsPredetor;
+    protected String PhotoHref;
+    protected  int[] coordinates;
+    protected String nextStep;
+    protected void setCoordinates(int[] coordinates){
+       this.coordinates=coordinates;
     };
+    protected void setNextStep(String nextStep){
+    this.nextStep=nextStep;
+    };
+    
     public abstract  boolean die();
 
 }
